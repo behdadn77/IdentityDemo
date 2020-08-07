ce7d5f1214edd582988f9f534f22e8b16dccc1a3
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-07 11:03:49 +0430

    added some areas for testing

---
+++ IdentityDemo/Areas/ContentManager/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemo.Areas.ContentManager.Controllers
+{
+    //[Authorize("AdminPolicy")]
+    [Area("ContentManager")]
+    [Route("ContentManager/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityDemo/Areas/SiteAdmin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemo.Areas.SiteAdmin.Controllers
+{
+    //[Authorize("AdminPolicy")]
+    [Area("SiteAdmin")]
+    [Route("SiteAdmin/[controller]/[action]")]
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

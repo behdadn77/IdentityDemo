a38b27b0c1f212e7640d481e95cba0ad4d7f6440
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-07 16:45:05 +0430

    Moved ErrorViewModel

--- IdentityDemo/Controllers/HomeController.cs
+++ IdentityDemo/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IdentityDemo.Models;
+using IdentityDemo.Models.ViewModels;
 
 namespace IdentityDemo.Controllers
 {
--- IdentityDemo/Models/ViewModels/ErrorViewModel.cs
+++ IdentityDemo/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace IdentityDemo.Models
+namespace IdentityDemo.Models.ViewModels
 {
     public class ErrorViewModel
     {
